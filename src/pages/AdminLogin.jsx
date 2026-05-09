2afa7d40b67896ce1a83c04fb08016276190e0d0
Sulok Pandey <np03cs4a220038@heraldcollege.edu.np>
2026-05-09 11:32:26 +0545

    Admin login api

---
+++ src/pages/AdminLogin.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import API from "../utils/api";
+
+const AdminLogin = () => {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await API.post("/admin/login", formData);
+
+            console.log(res.data);
+
+            localStorage.setItem("token", res.data.token);
+
+            alert("Login Successful");
+        } catch (error) {
+            console.log(error);
+            alert("Login Failed");
+        }
+    };
+
+    return (
+        <div>
+            <h1>Admin Login</h1>
+
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    onChange={handleChange}
+                />
+
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                />
+
+                <button type="submit">
+                    Login
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default AdminLogin;
